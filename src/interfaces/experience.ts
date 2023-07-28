4e139f34691613ff9ada3a4a29a84e7fe93b8213
Frederik Bode <thorbensen@gmail.com>
2023-07-28 15:33:59 +0200

    Update experience.ts

--- src/interfaces/experience.ts
+++ src/interfaces/experience.ts
@@ -2,7 +2,7 @@
 export interface Experience {
     title: string;
     company: string;
-    link: string;
+    link?: string;
     period: string;
     description: string;
     technologies: string[];
