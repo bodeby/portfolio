08560037442eaa12802755ee02ac11d9f1f709e0
Frederik Bode <thorbensen@gmail.com>
2023-08-11 16:57:06 +0200

    fix(edu_title): fixed mistake in education title

---
+++ src/routes/projects/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// Data
+	import projects from '$lib/data/projects.json';
+
+	// Components
+    import SubHeader from '$lib/components/layout/SubHeader.svelte';
+
+    let headerProps = {
+        title: 'Specific Side Project',
+        message: 'Most of the projects can be found on my github Account',
+        backlink: '/projects'
+    }
+</script>
+
+<!-- Sub page header -->
+<SubHeader {...headerProps}/>
+
+<!-- Side Projects -->
+<section class="gap-4 columns-1 md:columns-2 lg:columns-3">
+	{#each projects as exp}
+        <p>{exp.title}</p>
+	{/each}
+</section>
